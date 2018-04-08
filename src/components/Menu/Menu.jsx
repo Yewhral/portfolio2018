2770883869deac57b2d0edf85d50db4c695fcd1d
yewhral <zabrzyjewski@gmail.com>
2018-04-08 22:09:24 +0200

    Added classnames, added hamburger button for menu toggling below 768px (requires simple styling and  a button)

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -2,17 +2,40 @@ import React, { Component } from 'react';
 import MenuDescription from '../MenuDescription/MenuDescription';
 import MenuHeader from '../MenuHeader/MenuHeader';
 import MenuNavigation from '../MenuNavigation/MenuNavigation';
+import classNames from 'classnames';
 import styles from './Menu.module.scss';
 
 class Menu extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            mobileVisible: false,
+        };
+    }
+
+    hamburgerClick = () => {
+        this.setState({
+            mobileVisible: !this.state.mobileVisible,
+        })
+    };
+
     render() {
+        const { mobileVisible } = this.state;
         return (
-            <div className={styles.menuContainer}>
-                <MenuHeader />
-                <nav className={styles.navigation}>
-                    <MenuDescription />
-                    <MenuNavigation />
-                </nav>
+            <div className={styles.menuContent}>
+                <button
+                    className={classNames(styles.hamburgerButton)}
+                    onClick={this.hamburgerClick}
+                >
+                    Hamburger
+                </button>
+                <div className={classNames(styles.menuContainer, mobileVisible && styles.mobile)}>
+                    <MenuHeader />
+                    <nav className={styles.navigation}>
+                        <MenuDescription />
+                        <MenuNavigation />
+                    </nav>
+                </div>
             </div>
         );
     }
