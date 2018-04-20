bbdabbeaa2da20722cccead87197f99fca75393b
yewhral <zabrzyjewski@gmail.com>
2018-04-20 13:28:36 +0200

    Fixed backgrounds

--- src/resources/data/images.js
+++ src/resources/data/images.js
@@ -1,21 +1,24 @@
 export default
 [
-    // character sprites
-        // me
-            'http://res.cloudinary.com/doethe/image/upload/v1523625854/me0_m7bdk1.svg',
-            'http://res.cloudinary.com/doethe/image/upload/v1523625854/me1_ydlwzh.svg',
-            'http://res.cloudinary.com/doethe/image/upload/v1523625858/me2_a98aak.svg',
+// character sprites
+    // me
+        'http://res.cloudinary.com/doethe/image/upload/v1523625854/me0_m7bdk1.svg',
+        'http://res.cloudinary.com/doethe/image/upload/v1523625854/me1_ydlwzh.svg',
+        'http://res.cloudinary.com/doethe/image/upload/v1523625858/me2_a98aak.svg',
 
-        // recruiter
-            'http://res.cloudinary.com/doethe/image/upload/v1523625854/F1_h7gabo.svg',
-            'http://res.cloudinary.com/doethe/image/upload/v1523625854/F2_cxliey.svg',
+    // recruiter
+        'http://res.cloudinary.com/doethe/image/upload/v1523625854/F1_h7gabo.svg',
+        'http://res.cloudinary.com/doethe/image/upload/v1523625854/F2_cxliey.svg',
 
-        // CTO
-            'http://res.cloudinary.com/doethe/image/upload/v1523625854/M1_hkyuut.svg',
-            'http://res.cloudinary.com/doethe/image/upload/v1523625854/M2_xv90cn.svg',
+    // CTO
+        'http://res.cloudinary.com/doethe/image/upload/v1523625854/M1_hkyuut.svg',
+        'http://res.cloudinary.com/doethe/image/upload/v1523625854/M2_xv90cn.svg',
 
-    // backgrounds
-        'http://res.cloudinary.com/doethe/image/upload/v1523628051/donana-3287939_1920_yghr3a.jpg',
-        'http://res.cloudinary.com/doethe/image/upload/v1523634192/asd/pixiv-fantasia-12.jpg'
+// backgrounds
+    'http://res.cloudinary.com/doethe/image/upload/v1524221944/city_csft5b.jpg', // city
+    'http://res.cloudinary.com/doethe/image/upload/v1524221944/estate_gva0ad.jpg', // estate
+    'http://res.cloudinary.com/doethe/image/upload/v1524221943/chapter1q60_heugpm.jpg', //interview
+    'http://res.cloudinary.com/doethe/image/upload/v1524221943/officeApple_xym7hp.jpg', // good end
+    'http://res.cloudinary.com/doethe/image/upload/v1524221943/badEnd_enk0ji.jpg' // bad end
 
 ];
