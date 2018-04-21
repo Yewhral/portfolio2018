9c02c04a83daa37ae78039a59067ec939da49bd4
yewhral <zabrzyjewski@gmail.com>
2018-04-21 12:28:32 +0200

    Added request animation frame polyfill to support older browsers

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import 'core-js/es6/map';
 import 'core-js/es6/set';
 import 'core-js/es6/array';
+import 'raf/polyfill';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
---
+++ src/raf.js
@@ -0,0 +1,6 @@
+//https://medium.com/@barvysta/warning-react-depends-on-requestanimationframe-f498edd404b3
+const raf = global.requestAnimationFrame = (cb) => {
+    setTimeout(cb, 0);
+};
+
+export default raf;
